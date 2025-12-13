b3669536b46e2d2f938dd1890d6a0dd8eee73a52
David Glaude <dglaude@users.noreply.github.com>
2025-12-13 02:26:28 +0100

    Update mpconfigboard.h
    Move I2C

--- ports/espressif/boards/m5stack_cardputer/mpconfigboard.h
+++ ports/espressif/boards/m5stack_cardputer/mpconfigboard.h
@@ -8,12 +8,12 @@
 
 // Micropython setup
 
-#define MICROPY_HW_BOARD_NAME       "M5Stack Cardputer"
+#define MICROPY_HW_BOARD_NAME       "M5Stack Cardputer ADV"
 #define MICROPY_HW_MCU_NAME         "ESP32S3"
 
 #define MICROPY_HW_NEOPIXEL (&pin_GPIO21)
-#define DEFAULT_I2C_BUS_SCL (&pin_GPIO1)
-#define DEFAULT_I2C_BUS_SDA (&pin_GPIO2)
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO9)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO8)
 #define CIRCUITPY_BOARD_SPI         (2)
 #define CIRCUITPY_BOARD_SPI_PIN     {{.clock = &pin_GPIO36, .mosi = &pin_GPIO35}, \
                                      {.clock = &pin_GPIO40, .mosi = &pin_GPIO14, .miso = &pin_GPIO39}}
